dfea20fcc7be5db440c94f844dfc9177050918b6
PrinterOMG <PrinterOMG@ya.ru>
2021-09-29 19:00:03 +0400

    * Fix registration

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -11,9 +11,16 @@ async def bot_start(message: types.Message):
 
     with session() as s:
         if not s.query(User).get(message.chat.id):
-            user = User().id = message.from_user.id
-            filters = Filter().user_id = message.from_user.id
-            subscribes = Subscribe().user_id = message.from_user.id
-            last_jobs = LastJob().user_id = message.from_user.id
+            user = User()
+            user.id = message.from_user.id
+
+            filters = Filter()
+            filters.user_id = message.from_user.id
+
+            subscribes = Subscribe()
+            subscribes.user_id = message.from_user.id
+
+            last_jobs = LastJob()
+            last_jobs.user_id = message.from_user.id
 
             s.add_all([user, filters, subscribes, last_jobs])
